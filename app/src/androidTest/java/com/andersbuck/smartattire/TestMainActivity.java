7261fc8be295b2af6391e6bd11144eb71e539d9d
Anderson Buckles <buckles.anderson@gmail.com>
2016-02-26 23:09:43 -0500

    Add JUnit.

---
+++ app/src/androidTest/java/com/andersbuck/smartattire/TestMainActivity.java
@@ -0,0 +1,13 @@
+package com.andersbuck.smartattire;
+
+import junit.framework.TestCase;
+
+import org.junit.Test;
+
+public class TestMainActivity extends TestCase {
+
+    @Test
+    public void testTest() {
+        assertEquals("True", "True");
+    }
+}
